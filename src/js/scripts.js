5f2be84fb30e21fa7ce8cb2ea8d8c1a1d54167a6
sleepmanserg <sleepmanserg@gmail.com>
2021-06-09 18:15:08 +0300

    Search in header added, profile page added, cart page added, some fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,24 +12,66 @@
       theme.scrollToTab();
       theme.productActive();
       theme.inputPlusMinus();
+      theme.validateForms();
+      theme.floatingSearchBar();
+      theme.autocomplete();
+      theme.addToCartBtn();
       // theme.stickyCategoryHeader();
 
     },
+    /** Form validation */
+    validateForms: () => {
 
-    // stickyCategoryHeader: () => {
-    //   let categoryHeader = $('.category-header');
-    //   if (categoryHeader.length) {
-    //     $(window).on('scroll', function() {
-    //       if($(this).scrollTop() > 300) {
-    //         categoryHeader.addClass('header-stuck');
-    //         $('body').addClass('header-stucked');
-    //       } else {
-    //         categoryHeader.removeClass('header-stuck');
-    //         $('body').removeClass('header-stucked');
-    //       }
-    //     });
-    //   }
-    // },
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      let forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            form.classList.add('was-validated')
+          }, false);
+        });
+    },
+
+    /** Search bar show/hide */
+    floatingSearchBar: () => {
+      $(document).on('click', '.data-toggle', function () {
+        let $target = $($(this).data('target'));
+        let classes = $(this).data('classes');
+        let backdrop = $(this).data('backdrop');
+        let siteBackdrop = $('.site-backdrop');
+
+
+        $target.toggleClass(classes);
+
+        siteBackdrop.on('click', function (e) {
+          closeDisable();
+        });
+
+        return false;
+      });
+
+      function closeDisable() {
+        $('.site-backdrop, .search-hide, .search-form').removeClass('searchbarActive');
+      }
+    },
+
+    /** Search form autocomplete */
+    autocomplete: () => {
+      let datasrc = [
+        {label: 'Фила с тунцом', value: 'opt1'},
+        {label: 'Филадельфия', value: 'opt2'},
+      ];
+      const ac = new Autocomplete(document.getElementById('searchFormAutoComplete'), {
+        data: datasrc
+      });
+    },
 
     /** Scroll to active tab */
     scrollToTab: () => {
@@ -40,6 +82,7 @@
         }, '150');
       });
     },
+
     /** Add class for product card */
     productActive: () => {
       let categoryProductCard = $('.product__card');
@@ -66,6 +109,33 @@
         return false;
       });
     },
+
+    /** Add to cart button text change and class  */
+    addToCartBtn: () => {
+      let addToCartButton = $('.addToCartBtn');
+      addToCartButton.on("click", function() {
+        let el = $(this);
+        el.toggleClass('active');
+        el.text() == el.data("text-swap")
+          ? el.text(el.data("text-original"))
+          : el.text(el.data("text-swap"));
+      });
+    },
+
+    // stickyCategoryHeader: () => {
+    //   let categoryHeader = $('.category-header');
+    //   if (categoryHeader.length) {
+    //     $(window).on('scroll', function() {
+    //       if($(this).scrollTop() > 300) {
+    //         categoryHeader.addClass('header-stuck');
+    //         $('body').addClass('header-stucked');
+    //       } else {
+    //         categoryHeader.removeClass('header-stuck');
+    //         $('body').removeClass('header-stucked');
+    //       }
+    //     });
+    //   }
+    // },
 
     // toggler: () => {
     //   $(document).on('click', '.data-toggle', function () {
